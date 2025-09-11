ecace70fbf4a982f8366d3f382023e8813618ece
Mwesigwa-Guma <gumathomas097@gmail.com>
2025-09-11 05:17:17 +0100

    update user space program to handle bus voltage value correctly

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 const char* filename = "/dev/ina219_custom"; 
 
 int main(){
-    int fd = open(filename, O_RDWR);
+    int fd = open(filename, O_RDONLY);
     if(fd == -1){
         err(1, "could not open file %s", filename);
         return 1;
@@ -17,19 +17,15 @@ int main(){
 
     while(1){
         // struct input_event evt;
-        char buffer[128];
-        ssize_t bytes = read(fd, buffer, sizeof(buffer) - 1);
+        unsigned long val;
+        ssize_t bytes = read(fd, &val, sizeof(val));
         if(bytes <= 0){
             printf("failed to read sensor data %zd \n", bytes);
             close(fd);
             return 1;
         }
 
-        uint16_t shunt_voltage = buffer[0];
-        shunt_voltage |= shunt_voltage << 8;
-        shunt_voltage |= buffer[1];
-
-        printf("data from sensor %u\n", (unsigned int)shunt_voltage);
+        printf("data from sensor %lu\n", val);
         sleep(1);
     }
 
